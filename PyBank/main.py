6ec6fed2b2cb90c3e0f12ba9a5546a064f916cae
hkhalid15 <33817713+hkhalid15@users.noreply.github.com>
2018-03-13 19:10:22 -0400

    saved-work

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,45 +6,35 @@ import pandas as pd
 import numpy as np
 
 # In[2]:
-
 csvpath1 = (r"C:\Users\hakhalid\Documents\LEARNING\GW Data Analytics\Homeworks\python-challenge\PyBank\budget_data_1.csv")
 csvpath1_pd = pd.read_csv(csvpath1)
 csvpath1_pd.head()
 
 # In[3]:
-
 csvpath2 = (r"C:\Users\hakhalid\Documents\LEARNING\GW Data Analytics\Homeworks\python-challenge\PyBank\budget_data_2.csv")
 csvpath2_pd = pd.read_csv(csvpath2)
 csvpath2_pd.head
 
 # In[4]:
-
 frames = [csvpath1_pd, csvpath2_pd]
 
 # In[5]:
-
 mergecsv = pd.concat(frames)
 mergecsv.head()
 
 # In[6]:
-
 print("The total number of months is " + str(len(mergecsv) + 1))
 
 # In[7]:
-
 revenue = mergecsv['Revenue'].sum()
 
 # In[8]:
-
 print("The total revenue was "+ "$" + str(revenue))
 
 # In[9]:
-
 ave_change = revenue/(len(mergecsv) + 1)
 
 # In[10]:
-
-
 print ("The average change over the entire period was " + "$" + str(ave_change))
 
 # In[11]:
